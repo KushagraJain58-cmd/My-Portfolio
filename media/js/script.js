5deabb9d3f2edea4702459ed3cb5f6407a80adb2
Kushagra <ishaanjain112@gmail.com>
2022-03-14 22:36:16 +0530

    All slides

--- media/js/script.js
+++ media/js/script.js
@@ -436,7 +436,7 @@ for (var i = 0; i < learnMore.length; i++) {
   learnMore[i].addEventListener("click", function () {
     if (this.innerHTML == "Show Less") {
       this.nextElementSibling.style.display = "none";
-      this.innerHTML = "Learn More";
+      this.innerHTML = "Read More";
     } else {
       this.nextElementSibling.style.display = "block";
       this.innerHTML = "Show Less";
